f37cc0406ea99960569e4d75267467ed8f041aa3
yellow544 <31159133+yellow544@users.noreply.github.com>
2017-09-02 20:36:54 +0100

    gruntfile add

---
+++ gruntfile.js
@@ -0,0 +1,52 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+  	sass: {
+  		options: {
+  			sourceMap: true
+  		},
+  		dist: {
+  			files: {
+  				'main.css': 'main.sass'
+  			}
+  		}
+  	},
+
+  	imagemin: {
+  		dynamic: {
+  			files: [{
+  				expand: true,
+  				cwd: 'images/',
+  				src: ['**/*.{png,jpg,gif}'],
+  				dest: 'images/build/'
+  			}]
+  		}
+  	},
+
+   watch: {
+   		scripts: {
+   			files: ['sass/*.sass'],
+        	tasks: ['sass'],
+        	options: {
+            spawn: false,
+   			 },
+    	}
+	},
+
+   jshint: {
+      all: ['js/*.js']
+    } 
+  });
+  
+  // Load the plugins tasks
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  
+
+  // Default task(s).
+
+  grunt.registerTask('default', ['sass', 'imagemin', 'watch', 'jshint']);
+};
