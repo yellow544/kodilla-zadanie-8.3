a5dd2287c8ba69a5c53e14115a441b4e049afdc8
yellow544 <31159133+yellow544@users.noreply.github.com>
2017-09-05 20:42:42 +0100

    photo added/imagemin used

--- gruntfile.js
+++ gruntfile.js
@@ -18,7 +18,7 @@ module.exports = function(grunt) {
   			files: [{
   				expand: true,
   				cwd: 'images/',
-  				src: ['**/*.{png,jpg,gif}'],
+  				src: ['**/*.{png,JPG,gif}'],
   				dest: 'images/build/'
   			}]
   		}
